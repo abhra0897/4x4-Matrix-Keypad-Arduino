db0d1b1d8c023197aabfc44d4cdbd52ef76983f2
abhra0897 <abhra0897@gmail.com>
2018-07-17 12:13:41 +0530

    An example to use the lib
    In the example code, strings are formed using the returned characters and then converted to float.
    '*' is used as '.'
    'A' is used as + operator
    'B' is used as - operator
    'C' is used as CANCEL operator. It discards the last input
    'D' is used for ENTER operation. When pressed, it converts string to decimal (float) and assigns to a variable.

---
+++ example.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include "keyScan.h"
+
+#define ENTER 'D'
+#define CANCEL 'C'
+#define INCREMENT 'A'
+#define DECREMENT 'B'
+#define ZERO '#'
+
+char keypadInputChar[50] = "";
+float keypadInputFloat = 0.0;
+int counter = 0;
+
+
+void setup() 
+{
+    Serial.begin(9600);
+    setKeypadPins(2, 3, 4, 5, 6, 7, 8, 9);    
+}
+
+void loop() 
+{
+    char keyVal = scanKeyPress();
+
+    //if pressed key is not '\0', get keyval from lookup table
+    //Now do whatever with the value. Here we print it
+    if(keyVal != '\0')
+    {
+        if(keyVal != INCREMENT && keyVal != DECREMENT && keyVal != CANCEL && keyVal != ENTER && keyVal != ZERO)
+        {
+            keypadInputChar[counter] = keyVal;  //Form a string by appending characters
+            counter++;  //Increment the array index
+            Serial.print(keyVal);
+        }
+            
+        if(keyVal == ENTER)          //set the value as float from char
+        {
+            if(strlen(keypadInputChar) > 0)    //Set new value to float var only if the string is non zero. else prev. val stays
+                keypadInputFloat = atof(keypadInputChar);
+            Serial.println();
+            Serial.println(keypadInputFloat);
+            counter = 0;
+            memset(keypadInputChar, 0, strlen(keypadInputChar));
+        }
+        else if(keyVal == CANCEL)      //Sets the char input var to null but keeps the float input var untouched
+        {
+            counter = 0;
+            memset(keypadInputChar, 0, strlen(keypadInputChar));
+            Serial.println();
+        }
+        else if(keyVal == INCREMENT)      //Increments the last val of float input var by 1.0
+        {
+            counter = 0;
+            memset(keypadInputChar, 0, strlen(keypadInputChar));   //sets char input var to null first
+            keypadInputFloat++;
+            Serial.println(keypadInputFloat);
+        }
+        else if(keyVal == DECREMENT)     //Decrements the ... ^^^^^
+        {
+            counter = 0;
+            memset(keypadInputChar, 0, strlen(keypadInputChar));  ////sets char input var to null first
+            keypadInputFloat--;
+            if(keypadInputFloat < 0.0)   //if goes negative,
+                keypadInputFloat = 0.0;  //make it 0.0
+            Serial.println(keypadInputFloat);
+        }
+        else if(keyVal == ZERO)         //Perform CANCEL operation and also sets the float input var to 0.0
+        {
+            counter = 0;
+            memset(keypadInputChar, 0, strlen(keypadInputChar));
+            keypadInputFloat = 0.0;
+            Serial.println(keypadInputFloat);
+        }
+    }    
+}
